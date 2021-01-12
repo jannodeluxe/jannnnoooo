1f104b7a8d57df531e3a80c221a1605c94576161
jannodeluxe <46295868+jannodeluxe@users.noreply.github.com>
2021-01-12 13:57:57 +0100

    Add files via upload

---
+++ DSM7_enable_usb.sh
@@ -0,0 +1,20 @@
+Bash:
+#!/bin/sh
+# Enable USB Stick in DSM 7
+
+case "$1" in
+    start)
+        echo "Start"
+        echo "setting usbserial"
+        /sbin/modprobe usbserial
+        echo "setting ftdi_sio"
+        /sbin/modprobe ftdi_sio
+        echo "setting cdc-acm"
+        /sbin/modprobe cdc-acm
+        echo "Done..."
+        ;;
+   *)
+        echo "Usage: $1 {start}"
+        exit 1
+esac
+exit 0
